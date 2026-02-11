0de8e47f268ecd921276c01576cceaa447e0bc47
Francisco Artannyel Pinto Ferreira <artannyel@gmail.com>
2026-02-11 17:15:43 -0300

    feat: update name web app and add icons android/web

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Football Predictions',
+      title: 'Palpites Futebol',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1B5E20)),
       ),
